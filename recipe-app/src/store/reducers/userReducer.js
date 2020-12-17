568f8491df941eb9bd37782f9c4f930732aabff4
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-16 21:06:38 -0800

    added more keys to the initial state inthe user reducer

--- recipe-app/src/store/reducers/userReducer.js
+++ recipe-app/src/store/reducers/userReducer.js
@@ -1,9 +1,14 @@
 const initialState = {
-  isLoading: false,
+  name: "",
+  email: "",
+  username: "",
+  password: "",
+  loggedIn: false,
 };
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    //action for post request at the submit
     default: {
       return state;
     }
