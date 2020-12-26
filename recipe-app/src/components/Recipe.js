e1016a71b57743c2e648e90dbf2a6ca85d80e858
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-25 19:51:58 -0800

    entered fields in Recipe component

--- recipe-app/src/components/Recipe.js
+++ recipe-app/src/components/Recipe.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Recipe = () => {
+const Recipe = (props) => {
+  console.log("props in the Recipe component: ", props);
   return (
     <div>
-      <h3>Recipe</h3>
+      <h3>Title:</h3>
+      <p>Source: </p>
+      <h4>Ingredients: </h4>
+      {/* map list of Ingredients */}
+      <h4>Instructions: </h4>
+      <img alt="dish" />
+      <button>Edit</button>
+      <button>Delete</button>
     </div>
   );
 };
 
-export default Recipe;
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes,
+  };
+};
+
+export default connect(mapStateToProps, {})(Recipe);
