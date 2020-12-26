26230f5dd978a0fac4a4247a006afef4e8aad0a3
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-25 20:01:33 -0800

    passed down props to the Recipe component using useParams hook

--- recipe-app/src/components/Recipe.js
+++ recipe-app/src/components/Recipe.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { useParams, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 
 const Recipe = (props) => {
   console.log("props in the Recipe component: ", props);
+  const history = useHistory();
+  const params = useParams();
+
+  const recipe = props.recipes.find((rec) => rec.id === Number(params.id));
   return (
     <div>
-      <h3>Title:</h3>
-      <p>Source: </p>
+      <h3>Title: {recipe.title}</h3>
+      <p>Source: {recipe.source}</p>
       <h4>Ingredients: </h4>
       {/* map list of Ingredients */}
       <h4>Instructions: </h4>
       <img alt="dish" />
       <button>Edit</button>
       <button>Delete</button>
+      <p onClick={() => history.push("/dashboard")}>Back to Recipes</p>
     </div>
   );
 };
