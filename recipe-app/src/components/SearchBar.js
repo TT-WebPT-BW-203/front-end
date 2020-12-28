97c4f21f704e01784641680aa867912ed4928e06
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-28 11:01:34 -0800

    added handleChange and HandleSubmit functions to the search bar and the inputs are controlled

--- recipe-app/src/components/SearchBar.js
+++ recipe-app/src/components/SearchBar.js
@@ -2,11 +2,27 @@ import React, { useState } from "react";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
+  console.log(search);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch("");
+  };
+
   return (
     <div>
       <h3>Search for Recipes</h3>
-      <form>
-        <input placeholder="Enter recipe title to search here" name="search" />
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter recipe title to search here"
+          name="search"
+          value={search}
+          onChange={handleChange}
+        />
         <button>Search</button>
       </form>
     </div>
