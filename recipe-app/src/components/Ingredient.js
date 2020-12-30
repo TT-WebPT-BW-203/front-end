6a249ed12c9f0382c5c302810cf623404252d265
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-29 20:34:33 -0800

    added handleEdit and handleSave functions to the ingredient component, have not done recuder yet

--- recipe-app/src/components/Ingredient.js
+++ recipe-app/src/components/Ingredient.js
@@ -6,6 +6,8 @@ import { editIngredient } from "../store/actions";
 const Ingredient = (props) => {
   console.log("props in the Ingredient component: ", props);
 
+  const [edit, setEdit] = useState(false);
+  console.log("is editing? ", edit);
   const [newIngredient, setNewIngredient] = useState({
     name: "",
   });
@@ -13,15 +15,39 @@ const Ingredient = (props) => {
   const { id } = props.ingredient;
   console.log("id", id);
 
-  const handleEdit = (e) => {
-    props.editIngredient(id, newIngredient);
+  const handleChange = (e) => {
+    setNewIngredient({ name: e.target.value });
   };
+
+  const handleEdit = (e) => {
+    setEdit(true);
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    props.editIngredient(id, newIngredient);
+    setNewIngredient({
+      name: "",
+    });
+    setEdit(!edit);
+  };
+
   return (
-    <li>
-      {props.ingredient.name}
-      <button onClick={handleEdit}>edit</button>
-      <button>delete</button>
-    </li>
+    <div>
+      {edit ? (
+        <div>
+          <input value={newIngredient.name} onChange={handleChange} />{" "}
+          <button onClick={handleSave}>save</button>
+          <button onClick={() => setEdit(!edit)}>cancel edit</button>
+        </div>
+      ) : (
+        <li>
+          {props.ingredient.name}
+          <button onClick={handleEdit}>edit</button>
+          <button>delete</button>
+        </li>
+      )}
+    </div>
   );
 };
 
