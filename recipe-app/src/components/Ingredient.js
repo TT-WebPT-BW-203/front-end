d6d03417884c0f227f1abb855400f7f9f227828e
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-29 23:42:59 -0800

    cleaned up Ingredient component

--- recipe-app/src/components/Ingredient.js
+++ recipe-app/src/components/Ingredient.js
@@ -1,33 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
 import { editIngredient } from "../store/actions";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Ingredient = (props) => {
-  console.log("props in the Ingredient component: ", props);
-
   const [edit, setEdit] = useState(false);
-  console.log("is editing? ", edit);
   const [newIngredient, setNewIngredient] = useState({
     name: "",
   });
-  const [ingredientFromGet, setIngredientFromGet] = useState("");
-  console.log("ingredientFromGet", ingredientFromGet);
 
-  const params = useParams();
   const { id } = props.ingredient;
-  console.log("id", id);
-
-  useEffect(() => {
-    axiosWithAuth()
-      .get(`/api/ingredients/${params.id}`)
-      .then((res) => {
-        console.log("res in the get of ingrd by id: ", res.data.name);
-        setIngredientFromGet(res.data.name);
-      })
-      .catch((err) => console.log(err));
-  }, [props]);
 
   const handleChange = (e) => {
     setNewIngredient({ name: e.target.value });
