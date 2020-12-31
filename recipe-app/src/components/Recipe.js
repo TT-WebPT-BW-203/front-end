67a78f56491c99869cf6a068a7e805a3f2fa406b
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-31 15:47:47 -0800

    Gutted the recipe component

--- recipe-app/src/components/Recipe.js
+++ recipe-app/src/components/Recipe.js
@@ -19,15 +19,7 @@ import {
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Recipe = (props) => {
-  console.log("props in the Recipe component (from the GLOBAL STATE): ", props);
-
-  const [rehydrate, setRehydrate] = useState([{}]);
-  console.log("Recipe: rehydrate: ", rehydrate);
-  const [instructionsSideEffects, setInstructionsSideEffects] = useState([{}]);
-  console.log(
-    "Recipe: instructionsSideEffects: useEffect(): ",
-    instructionsSideEffects
-  );
+  console.log();
   const history = useHistory();
   const params = useParams();
 
@@ -37,83 +29,7 @@ const Recipe = (props) => {
     recipe
   );
 
-  useEffect(() => {
-    axiosWithAuth()
-      .get(`/api/recipes/${params.id}`)
-      .then((res) => {
-        console.log("getrecipesbyID res: ", res.data);
-        setRehydrate(res.data.ingredients);
-        setInstructionsSideEffects(res.data.instructions);
-      })
-      .catch((err) => console.log(err));
-  }, [props.ingredients, props.instructions]);
-
-  return (
-    <RecipeContainer>
-      <LeftContent>
-        <RecipeTitle>Title: {recipe.title}</RecipeTitle>
-        <p>Source: {recipe.source}</p>
-        <p>Category: {recipe.category}</p>
-        <h3>Ingredients: </h3>
-
-        {rehydrate.map((ing) => (
-          <div>
-            <li>
-              {ing.name} <button>edit</button>
-              <button>delete</button>
-            </li>
-          </div>
-        ))}
-        <Button
-          onClick={() => history.push(`/recipe/ingredients/${recipe.id}`)}
-        >
-          Add Ingredients
-        </Button>
-        {instructionsSideEffects.map((inst) => (
-          <div>
-            <p>Step {inst.step}:</p>
-            <p>Details {inst.details}</p>
-          </div>
-        ))}
-        <Button
-          onClick={() => {
-            history.push(`/instructions/${recipe.id}`);
-          }}
-        >
-          Instructions
-        </Button>
-      </LeftContent>
-      <RightContent>
-        {recipe.image === "" ? (
-          <Image src={img_placeholder} alt="Missing dish image" />
-        ) : (
-          <Image src={recipe.image} alt="Dish" />
-        )}
-
-        <Button
-          onClick={() => {
-            history.push(`/recipe/${recipe.id}/update_recipe`);
-          }}
-        >
-          Edit
-        </Button>
-        <Button
-          onClick={() => {
-            props.deleteRecipe(params.id);
-            history.goBack();
-          }}
-        >
-          Delete
-        </Button>
-        <Link
-          style={{ marginTop: ".5rem" }}
-          onClick={() => history.push("/dashboard")}
-        >
-          Back to Recipes
-        </Link>
-      </RightContent>
-    </RecipeContainer>
-  );
+  return <div>recipe component</div>;
 };
 
 const mapStateToProps = (state) => {
