a2462570be4a6fb6003f0fd666985bafd660ac00
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-31 19:07:55 -0800

    created handle submit

--- recipe-app/src/components/InstructionsForm.js
+++ recipe-app/src/components/InstructionsForm.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
 
-const InstructionsForm = () => {
-  return <div>this is the instructions component</div>;
+const InstructionsForm = (props) => {
+  const { push } = useHistory();
+  const [instruction, setInstruction] = useState({
+    step: "",
+    details: "",
+  });
+  console.log("InstructionsForm: instruction:", instruction);
+
+  const handleChange = (e) => {
+    setInstruction({
+      ...instruction,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInstruction({
+      step: "",
+      details: "",
+    });
+  };
+
+  return (
+    <div>
+      this is the instructions component
+      <form>
+        <input name="step" value={instruction.step} onChange={handleChange} />
+        <input
+          name="details"
+          value={instruction.details}
+          onChange={handleChange}
+        />
+        <button>save Step</button>
+        <button>cancel step</button>
+      </form>
+    </div>
+  );
 };
 
-export default InstructionsForm;
+export default connect(null, {})(InstructionsForm);
