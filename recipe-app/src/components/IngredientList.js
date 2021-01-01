da5069089b43cbe11bcad92e346df7229ec56173
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-31 16:08:14 -0800

    the input form the ingredient form is being rendered in the inside the ingredient list in the Recipe.js

--- recipe-app/src/components/IngredientList.js
+++ recipe-app/src/components/IngredientList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { connect } from "react-redux";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const IngredientList = (props) => {
   console.log("props in the IngredientsList component: ", props);
-
   const { id } = useParams();
+
+  const [rehydratedIngredients, setRehydratedIngredients] = useState([]);
+  console.log("IngredientList: rehydratedIngredients: ", rehydratedIngredients);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/api/recipes/${id}	`)
+      .then((res) => {
+        console.log("IngrideintList: useEffect(): res: ", res.data);
+        setRehydratedIngredients(res.data.ingredients);
+      })
+      .catch((err) => console.log(err));
+  }, [props.ingredients]);
 
   const list = props.ingredients.filter((ing) => ing.recipe_id === Number(id));
   console.log("list const", list);
