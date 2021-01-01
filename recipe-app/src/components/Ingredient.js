4ac267cfab78df193c622823aedd30725e82ef32
Katiuska Alicea <katiuskaalicea@gmail.com>
2020-12-31 17:10:15 -0800

    connected actions and ingredients sate to the Ingredient.js

--- recipe-app/src/components/Ingredient.js
+++ recipe-app/src/components/Ingredient.js
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+import { editIngredient, deleteIngredient } from "../store/actions";
 
-const Ingredient = () => {
+const Ingredient = (props) => {
+  console.log("Ingredient: props: ", props);
+  const { id } = useParams();
   const [isEditing, setIsEditing] = useState(false);
+  const [newIngredient, setNewIngredient] = useState({
+    name: "",
+  });
 
+  const handleChange = (e) => {
+    setNewIngredient({ name: e.target.value });
+  };
   const handleEditButton = () => {
     setIsEditing(!isEditing);
   };
+
+  const handleSave = () => {};
 
   return (
     <div>
       {isEditing ? (
         <div>
-          <input />
+          <input value={newIngredient.name} onChange={handleChange} />
           <button>save</button>
           <button onClick={() => setIsEditing(!isEditing)}>cancel</button>
         </div>
@@ -24,5 +37,11 @@ const Ingredient = () => {
     </div>
   );
 };
-
-export default Ingredient;
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.ingredients,
+  };
+};
+export default connect(mapStateToProps, { editIngredient, deleteIngredient })(
+  Ingredient
+);
